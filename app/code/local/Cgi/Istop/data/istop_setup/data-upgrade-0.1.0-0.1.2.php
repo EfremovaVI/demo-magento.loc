6c06a69dfb5b17043c8d57967f2dad1cce29e2d7
Evi <efremova.vasilina@mail.ru>
2016-03-14 13:03:30 +0200

    Task 1: Top3 widget

---
+++ app/code/local/Cgi/Istop/data/istop_setup/data-upgrade-0.1.0-0.1.2.php
@@ -0,0 +1,29 @@
+<?php
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+$installer->startSetup();
+
+$identifier = 'top_product';
+$title = 'Top product';
+$content = '{{widget type="istop/topproduct" widget_name="Top product" count_product="3"}}';
+
+Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
+
+$staticBlock = array(
+    'title' => $title,
+    'identifier' => $identifier,
+    'content' => $content,
+    'is_active' => 1,
+    'stores' => array(0)
+);
+
+$block= Mage::getModel('cms/block')->load($staticBlock['identifier']);
+if ($block->getId()) {
+    $block->setContent($staticBlock['content']);
+    $block->setTitle($staticBlock['title']);
+    $block->save();
+} else {
+    Mage::getModel('cms/block')->setData($staticBlock )->save();
+}
+
+$installer->endSetup();
