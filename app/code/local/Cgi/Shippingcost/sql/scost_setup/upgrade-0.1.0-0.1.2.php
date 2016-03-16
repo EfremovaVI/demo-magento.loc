ad051cd13ad4f1e6f5eef5a2064d390d4cee89d8
Evi <efremova.vasilina@mail.ru>
2016-03-16 12:33:15 +0200

    Task 3: Additional shipping cost

--- app/code/local/Cgi/Shippingcost/sql/scost_setup/upgrade-0.1.0-0.1.2.php
+++ app/code/local/Cgi/Shippingcost/sql/scost_setup/upgrade-0.1.0-0.1.2.php
@@ -25,28 +25,6 @@ $installer->getConnection()
     );
 
 $installer->getConnection()
-    ->addColumn($installer->getTable('sales/quote_address'),
-        'total_shippingcost_amount',
-        array(
-            'type' => Varien_Db_Ddl_Table::TYPE_DECIMAL,
-            'length' => '12,4',
-            'nullable'  => true,
-            'comment' => 'Total Shipping Cost Amount',
-        )
-    );
-
-$installer->getConnection()
-    ->addColumn($installer->getTable('sales/invoice'),
-        'total_shippingcost_amount',
-        array(
-            'type' => Varien_Db_Ddl_Table::TYPE_DECIMAL,
-            'length' => '12,4',
-            'nullable'  => true,
-            'comment' => 'Total Shipping Cost Amount',
-        )
-    );
-
-$installer->getConnection()
     ->addColumn($installer->getTable('sales/order'),
         'total_shippingcost_amount',
         array(
@@ -56,5 +34,40 @@ $installer->getConnection()
             'comment' => 'Total Shipping Cost Amount',
         )
     );
+
+//$installer->getConnection()
+//    ->addColumn($installer->getTable('sales/quote_address'),
+//        'total_shippingcost_amount',
+//        array(
+//            'type' => Varien_Db_Ddl_Table::TYPE_DECIMAL,
+//            'length' => '12,4',
+//            'nullable'  => true,
+//            'comment' => 'Total Shipping Cost Amount',
+//        )
+//    );
+//
+//$installer->getConnection()
+//    ->addColumn($installer->getTable('sales/invoice'),
+//        'total_shippingcost_amount',
+//        array(
+//            'type' => Varien_Db_Ddl_Table::TYPE_DECIMAL,
+//            'length' => '12,4',
+//            'nullable'  => true,
+//            'comment' => 'Total Shipping Cost Amount',
+//        )
+//    );
+//
+//$installer->getConnection()
+//    ->addColumn($installer->getTable('sales/creditmemo'),
+//        'total_shippingcost_amount',
+//        array(
+//            'type' => Varien_Db_Ddl_Table::TYPE_DECIMAL,
+//            'length' => '12,4',
+//            'nullable'  => true,
+//            'comment' => 'Total Shipping Cost Amount',
+//        )
+//    );
+
+
 
 $installer->endSetup();
